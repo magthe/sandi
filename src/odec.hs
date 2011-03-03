34cab2e6be4e0a8d85fe9b1012e28066049e2ef4
Magnus Therning <magnus@therning.org>
2011-03-03 12:14:28 +0000

    First working implementation of odec using an enumeratee.  Only for base64 and base32 though.

--- src/odec.hs
+++ src/odec.hs
@@ -22,45 +22,89 @@ module Main
     ( main
     ) where
 
--- import qualified Codec.Binary.Base64 as Base64
--- import qualified Codec.Binary.Base32 as Base32
-
 import Paths_omnicodec (version)
 
+import Codec.Binary.Base64 as B64
+import qualified Codec.Binary.Base32 as B32
+import Data.ByteString.Iteratee
+import Data.ByteString.Iteratee.Internals
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import System.Console.CmdArgs
-import Data.Version
-import Data.Enumerator as E
-import Data.Enumerator.Binary as EB
+import Data.Version(showVersion)
 import System.IO
-import Control.Monad.IO.Class
-import Data.ByteString.Lazy
+import Data.Maybe
 
--- {{{1 command line arguments
-data Codec = B64 | B32
-    deriving(Show, Data, Typeable)
-
-data MyArgs = MyArgs { argOutput :: FilePath, argCodec :: Codec }
-    deriving(Show, Data, Typeable)
-
+-- {{{1 command line options
 ver :: String
 ver = "omnicode decode (odec) " ++ (showVersion version)
     ++ "\nCopyright 2007-2011 Magnus Therning <magnus@therning.org>"
 
+data Codec = B64 | B32
+    deriving(Show, Eq, Data, Typeable)
+
+codecMap =
+    [ (B64, B64.decodeInc)
+    , (B32, B32.decodeInc)
+    ]
+
+data MyArgs = MyArgs { argInput :: Maybe FilePath, argOutput :: Maybe FilePath, argCodec :: Codec }
+    deriving(Show, Data, Typeable)
+
 myArgs :: MyArgs
 myArgs = MyArgs
-    { argOutput = def &= name "o" &= explicit &= typFile &= help "output to file"
-    , argCodec = B64 &= name "c" &= typ "CODEC" &= help "codec"
+    { argInput = Nothing &= name "i" &= name "in" &= explicit &= typFile &= help "read encoded data from file"
+    , argOutput = Nothing &= name "o" &= name "out" &= explicit &= typFile &= help "write decoded data to file"
+    , argCodec = B64 &= name "c" &= name "codec" &= explicit &= typ "CODEC" &= help "codec b64, b32 (b64)"
     } &= summary ver
+
+-- {{{1 decode enumeratee
+decEnumeratee :: Monad m => (DecIncData String -> DecIncRes String) -> Enumeratee m a
+decEnumeratee decF iter = Iteratee $ step decF iter
+    where
+        step f i Eof = let
+                d = f DDone
+            in do
+                case d of
+                    DFinal dbs _ -> do
+                        ir <- runIteratee i (Chunk $ BS.pack dbs)
+                        case ir of
+                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
+                            NeedAnotherChunk i' -> do
+                                ir' <- runIteratee i' Eof
+                                case ir' of
+                                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
+                                    NeedAnotherChunk _ -> error "decEnumeratee: inner iteratee diverges on Eof"
+                    DFail _ _ -> error "decEnumeratee: decoder failed on data"
+                    DPart _ _ -> error "decEnumeratee: decoder didn't terminate on DDone"
+
+        step f i (Chunk bs) = let
+                d = f (DChunk $ BSC.unpack bs)
+            in do
+                case d of
+                    DFinal dbs r -> do
+                        ir <- runIteratee i (Chunk $ BS.pack dbs)
+                        case ir of
+                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a (Chunk $ BSC.pack r)) (Chunk $ BSC.pack r)
+                            NeedAnotherChunk i' -> do
+                                ir' <- runIteratee i' Eof
+                                case ir' of
+                                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
+                                    NeedAnotherChunk _ -> error "decEnumeratee: inner iteratee diverges on Eof"
+                    DFail _ _ -> error "decEnumeratee: decoder failed on data"
+                    DPart dbs f' -> do
+                        ir <- runIteratee i (Chunk $ BS.pack dbs)
+                        case ir of
+                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a (Chunk BS.empty)) (Chunk BS.empty)
+                            NeedAnotherChunk i' -> return $ NeedAnotherChunk $ Iteratee $ step f' i'
 
 -- {{{1 main
 main :: IO ()
 main = do
-    cmdArgs myArgs >>= \ a -> print a
-    d <- run_ $ enumHandle 2048 stdin $$ encIter
-    print d
-
--- {{{1 iteratee
-encIter = do
-    d <- EB.take 8
-    let d' = unpack d
-    return d'
+    cmdArgs myArgs >>= \ a -> do
+    hIn <- maybe (return stdin) (\ fn -> openFile fn ReadMode) (argInput a)
+    hOut <- maybe (return stdout) (\ fn -> openFile fn WriteMode) (argOutput a)
+    let dI = fromJust $ lookup (argCodec a) codecMap
+    (enumHandle hIn $ decEnumeratee dI (sinkHandle hOut)) >>= run >>= run
+    hClose hOut
