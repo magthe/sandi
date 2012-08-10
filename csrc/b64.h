1ebeeae7fa3a220717ebc32633e8f63814a19f03
Magnus Therning <magnus@therning.org>
2012-08-10 22:23:40 +0200

    Add base64 decoding.
    Still no proper tests :(
    
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- csrc/b64.h
+++ csrc/b64.h
@@ -9,5 +9,10 @@ void b64_enc_part(uint8_t const *src, size_t srclen,
     uint8_t const **rem, size_t *remlen);
 int b64_enc_final(uint8_t const *src, size_t srclen,
     uint8_t *dst, size_t *dstlen);
+int b64_dec_part(uint8_t const *src, size_t srclen,
+    uint8_t *dst, size_t *dstlen,
+    uint8_t const **rem, size_t *remlen);
+int b64_dec_final(uint8_t const *src, size_t srclen,
+    uint8_t *dst, size_t *dstlen);
 
 #endif
--- src/Codec/Binary/Base64.hs
+++ src/Codec/Binary/Base64.hs
@@ -2,6 +2,8 @@
 module Codec.Binary.Base64
     ( b64encode_part
     , b64encode_final
+    , b64decode_part
+    , b64decode_final
     ) where
 
 import Foreign
@@ -20,6 +22,12 @@ foreign import ccall "static b64.h b64_enc_part"
 
 foreign import ccall "static b64.h b64_enc_final"
     c_b64_enc_final :: Ptr Word8 -> CSize -> Ptr Word8 -> Ptr CSize -> IO CInt
+
+foreign import ccall "static b64.h b64_dec_part"
+    c_b64_dec_part :: Ptr Word8 -> CSize -> Ptr Word8 -> Ptr CSize -> Ptr (Ptr Word8) -> Ptr CSize -> IO CInt
+
+foreign import ccall "static b64.h b64_dec_final"
+    c_b64_dec_final :: Ptr Word8 -> CSize -> Ptr Word8 -> Ptr CSize -> IO CInt
 
 b64encode_part :: BS.ByteString -> (BS.ByteString, BS.ByteString)
 b64encode_part bs = U.unsafePerformIO $ unsafeUseAsCStringLen bs $ \ (inBuf, inLen) -> do
@@ -43,6 +51,38 @@ b64encode_final bs = U.unsafePerformIO $ unsafeUseAsCStringLen bs $ \ (inBuf, in
     outBuf <- mallocBytes 4
     alloca $ \ pOutLen -> do
         r <- c_b64_enc_final (castPtr inBuf) (castEnum inLen) outBuf pOutLen
+        if r /= 0
+            then return Nothing
+            else do
+                outLen <- peek pOutLen
+                outBs <- unsafePackCStringFinalizer outBuf (castEnum outLen) (free outBuf)
+                return $ Just outBs
+
+-- todo: too much memory is used when there's an error
+b64decode_part :: BS.ByteString -> Either (BS.ByteString, BS.ByteString) (BS.ByteString, BS.ByteString)
+b64decode_part bs = U.unsafePerformIO $ unsafeUseAsCStringLen bs $ \ (inBuf, inLen) -> do
+    let maxOutLen = inLen `div` 4 * 3
+    outBuf <- mallocBytes maxOutLen
+    alloca $ \ pOutLen ->
+        alloca $ \ pRemBuf ->
+            alloca $ \ pRemLen -> do
+                r <- c_b64_dec_part (castPtr inBuf) (castEnum inLen)
+                    outBuf pOutLen pRemBuf pRemLen
+                outLen <- peek pOutLen
+                remBuf <- peek pRemBuf
+                remLen <- peek pRemLen
+                remBs <- BS.packCStringLen (castPtr remBuf, castEnum remLen)
+                outBs <- unsafePackCStringFinalizer outBuf (castEnum outLen) (free outBuf)
+                if r == 0
+                    then return $ Right (outBs, remBs)
+                    else return $ Left (outBs, remBs)
+
+-- todo: too much memory is used when 0 or 1 byte is encoded
+b64decode_final :: BS.ByteString -> Maybe BS.ByteString
+b64decode_final bs = U.unsafePerformIO $ unsafeUseAsCStringLen bs $ \ (inBuf, inLen) -> do
+    outBuf <- mallocBytes 2
+    alloca $ \ pOutLen -> do
+        r <- c_b64_dec_final (castPtr inBuf) (castEnum inLen) outBuf pOutLen
         if r /= 0
             then return Nothing
             else do
