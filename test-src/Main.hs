570ede81ea7753c58d5faf9e0722c50f8c379f69
Magnus Therning <magnus@therning.org>
2012-08-14 23:44:17 +0200

    Add tests for two last chars in base64 (+/).
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- test-src/Main.hs
+++ test-src/Main.hs
@@ -9,6 +9,7 @@ import Data.ByteString
 import Codec.Binary.Base64
 
 case_b64encode = do
+    -- foobar
     empty @=? encode empty
     pack [90,103,61,61] @=? encode (pack [102])
     pack [90,109,56,61] @=? encode (pack [102,111])
@@ -16,8 +17,11 @@ case_b64encode = do
     pack [90,109,57,118,89,103,61,61] @=? encode (pack [102,111,111,98])
     pack [90,109,57,118,89,109,69,61] @=? encode (pack [102,111,111,98,97])
     pack [90,109,57,118,89,109,70,121] @=? encode (pack [102,111,111,98,97,114])
+    -- /++/
+    pack [47,43,43,47] @=? encode (pack [255,239,191])
 
 case_b64decode = do
+    -- foobar
     Right empty @=? decode empty 
     (Right $ pack [102]) @=? decode (pack [90,103,61,61]) 
     (Right $ pack [102,111]) @=? decode (pack [90,109,56,61]) 
@@ -25,6 +29,8 @@ case_b64decode = do
     (Right $ pack [102,111,111,98]) @=? decode (pack [90,109,57,118,89,103,61,61]) 
     (Right $ pack [102,111,111,98,97]) @=? decode (pack [90,109,57,118,89,109,69,61]) 
     (Right $ pack [102,111,111,98,97,114]) @=? decode (pack [90,109,57,118,89,109,70,121]) 
+    -- /++/
+    (Right $ pack [255,239,191]) @=? decode (pack [47,43,43,47])
 
 tests = [$(testGroupGenerator)]
 
