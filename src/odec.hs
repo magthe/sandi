e54024e7a32b6e2927d40f41cc9a6bfb535bd9aa
Magnus Therning <magnus@therning.org>
2012-12-28 10:55:08 +0100

    Total rewrite to use sandi.
    A complete rewrite of the tools to use the new coding library sandi.
    
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/odec.hs
+++ src/odec.hs
@@ -2,7 +2,7 @@
 
 {-
 odec - command line utility for data decoding
-Copyright (C) 2008  Magnus Therning
+Copyright (C) 2012  Magnus Therning
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,11 +18,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
-module Main
-    ( main
-    ) where
+module Main where
 
 import Paths_omnicodec (version)
+
+import Control.Exception
+import Control.Monad
+import Data.ByteString as BS
+import Data.Maybe
+import Data.Version(showVersion)
+import System.Console.CmdArgs
+import System.Exit
+import System.IO as SIO
 
 import Codec.Binary.Base64 as B64
 import qualified Codec.Binary.Base64Url as B64U
@@ -30,42 +37,33 @@ import qualified Codec.Binary.Base32 as B32
 import qualified Codec.Binary.Base32Hex as B32H
 import qualified Codec.Binary.Base16 as B16
 import qualified Codec.Binary.Base85 as B85
-import qualified Codec.Binary.PythonString as PS
 import qualified Codec.Binary.QuotedPrintable as QP
-import qualified Codec.Binary.Url as Url
 import qualified Codec.Binary.Uu as Uu
 import qualified Codec.Binary.Xx as Xx
-import Data.ByteString.Iteratee
-import Data.ByteString.Iteratee.Internals
-
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
-import System.Console.CmdArgs
-import Data.Version(showVersion)
-import System.IO
-import Data.Maybe
+-- import qualified Codec.Binary.PythonString as PS
+-- import qualified Codec.Binary.Url as Url
 
 -- {{{1 command line options
 ver :: String
 ver = "omnicode decode (odec) " ++ (showVersion version)
-    ++ "\nCopyright 2007-2011 Magnus Therning <magnus@therning.org>"
+    ++ "\nCopyright 2012 Magnus Therning <magnus@therning.org>"
 
-data Codec = B64 | B64U | B32 | B32H | B16 | B85 | PS | QP | Url | Uu | Xx
+data Codec = B64 | B64U | B32 | B32H | B16 | B85 | QP | Uu | Xx
     deriving(Show, Eq, Data, Typeable)
 
-codecMap :: [(Codec, DecIncData String -> DecIncRes String)]
+codecMap :: [(Codec, BS.ByteString -> Either (BS.ByteString, BS.ByteString) BS.ByteString)]
 codecMap =
-    [ (B64, B64.decodeInc)
-    , (B64U, B64U.decodeInc)
-    , (B32, B32.decodeInc)
-    , (B32H, B32H.decodeInc)
-    , (B16, B16.decodeInc)
-    , (B85, B85.decodeInc)
-    , (PS, PS.decodeInc)
-    , (QP, QP.decodeInc)
-    , (Url, Url.decodeInc)
-    , (Uu, Uu.decodeInc)
-    , (Xx, Xx.decodeInc)
+    [ (B64, B64.decode)
+    , (B64U, B64U.decode)
+    , (B32, B32.decode)
+    , (B32H, B32H.decode)
+    , (B16, B16.decode)
+    , (B85, B85.decode)
+    , (QP, QP.decode)
+    , (Uu, Uu.decode)
+    , (Xx, Xx.decode)
+    -- , (PS, PS.decode)
+    -- , (Url, Url.decode)
     ]
 
 data MyArgs = MyArgs { argInput :: Maybe FilePath, argOutput :: Maybe FilePath, argCodec :: Codec }
@@ -84,59 +82,38 @@ myArgs = MyArgs
         , " b32h - base32hex"
         , " b16  - base16"
         , " b85  - base85"
-        , " ps   - python string escaping"
         , " qp   - quoted printable"
-        , " url  - url encoding"
         , " uu   - uu encoding"
         , " xx   - xx encoding"
+        -- , " ps   - python string escaping"
+        -- , " url  - url encoding"
         ]
-
--- {{{1 decode enumeratee
-decEnumeratee :: Monad m => (DecIncData String -> DecIncRes String) -> Enumeratee m a
-decEnumeratee decF iter = Iteratee $ step decF iter
-    where
-        step f i Eof = let
-                d = f DDone
-            in do
-                case d of
-                    DFinal dbs _ -> do
-                        ir <- runIteratee i (Chunk $ BS.pack dbs)
-                        case ir of
-                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
-                            NeedAnotherChunk i' -> do
-                                ir' <- runIteratee i' Eof
-                                case ir' of
-                                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
-                                    NeedAnotherChunk _ -> error "decEnumeratee: inner iteratee diverges on Eof"
-                    DFail _ _ -> error "decEnumeratee: decoder failed on data"
-                    DPart _ _ -> error "decEnumeratee: decoder didn't terminate on DDone"
-
-        step f i (Chunk bs) = let
-                d = f (DChunk $ BSC.unpack bs)
-            in do
-                case d of
-                    DFinal dbs r -> do
-                        ir <- runIteratee i (Chunk $ BS.pack dbs)
-                        case ir of
-                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a (Chunk $ BSC.pack r)) (Chunk $ BSC.pack r)
-                            NeedAnotherChunk i' -> do
-                                ir' <- runIteratee i' Eof
-                                case ir' of
-                                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
-                                    NeedAnotherChunk _ -> error "decEnumeratee: inner iteratee diverges on Eof"
-                    DFail _ _ -> error "decEnumeratee: decoder failed on data"
-                    DPart dbs f' -> do
-                        ir <- runIteratee i (Chunk $ BS.pack dbs)
-                        case ir of
-                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a (Chunk BS.empty)) (Chunk BS.empty)
-                            NeedAnotherChunk i' -> return $ NeedAnotherChunk $ Iteratee $ step f' i'
 
 -- {{{1 main
 main :: IO ()
 main = do
     cmdArgs myArgs >>= \ a -> do
-    hIn <- maybe (return stdin) (\ fn -> openFile fn ReadMode) (argInput a)
-    hOut <- maybe (return stdout) (\ fn -> openFile fn WriteMode) (argOutput a)
-    let dI = fromJust $ lookup (argCodec a) codecMap
-    (enumHandle hIn $ decEnumeratee dI (sinkHandle hOut)) >>= run >>= run
-    hClose hOut
+    let encFunc = lookup (argCodec a) codecMap
+    withMaybeFile (argInput a) ReadMode $ \ inputFile ->
+        withMaybeFile (argOutput a) WriteMode $ \ outputFile ->
+            decodeFile (fromJust encFunc) inputFile outputFile
+
+withMaybeFile :: Maybe FilePath -> IOMode -> (Handle -> IO r) -> IO r
+withMaybeFile fn mode func = let
+        dH = if mode == ReadMode
+            then stdin
+            else stdout
+    in bracket 
+        (maybe (return dH) (flip openFile mode) fn)
+        hClose
+        func
+
+decodeFile :: (BS.ByteString -> Either a BS.ByteString) -> Handle -> Handle -> IO ()
+decodeFile decFunc inF outF = let
+        writeToFile d = either
+            (const $ SIO.hPutStrLn stderr "Decoding failed!" >> exitFailure)
+            (BS.hPut outF)
+            (decFunc d)
+    in do
+        inData <- BS.hGet inF 4096
+        unless (BS.null inData) $ writeToFile inData >> decodeFile decFunc inF outF
--- src/oenc.hs
+++ src/oenc.hs
@@ -2,7 +2,7 @@
 
 {-
 oenc - command line utility for data encoding
-Copyright (C) 2008  Magnus Therning
+Copyright (C) 2012  Magnus Therning
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,48 +22,47 @@ module Main where
 
 import Paths_omnicodec (version)
 
+import Control.Exception
+import Control.Monad
+import Data.ByteString as BS
+import Data.Maybe
+import Data.Version(showVersion)
+import System.Console.CmdArgs
+import System.IO
+
 import Codec.Binary.Base64 as B64
 import qualified Codec.Binary.Base64Url as B64U
 import qualified Codec.Binary.Base32 as B32
 import qualified Codec.Binary.Base32Hex as B32H
 import qualified Codec.Binary.Base16 as B16
 import qualified Codec.Binary.Base85 as B85
-import qualified Codec.Binary.PythonString as PS
 import qualified Codec.Binary.QuotedPrintable as QP
-import qualified Codec.Binary.Url as Url
 import qualified Codec.Binary.Uu as Uu
 import qualified Codec.Binary.Xx as Xx
-import Data.ByteString.Iteratee
-import Data.ByteString.Iteratee.Internals
-
-import Data.Version(showVersion)
-import System.Console.CmdArgs
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
-import System.IO
-import Data.Maybe
+-- import qualified Codec.Binary.PythonString as PS
+-- import qualified Codec.Binary.Url as Url
 
 -- {{{1 command line options
 ver :: String
 ver = "omnicode encode (oenc) " ++ (showVersion version)
-    ++ "\nCopyright 2007-2011 Magnus Therning <magnus@therning.org>"
+    ++ "\nCopyright 2012 Magnus Therning <magnus@therning.org>"
 
-data Codec = B64 | B64U | B32 | B32H | B16 | B85 | PS | QP | Url | Uu | Xx
+data Codec = B64 | B64U | B32 | B32H | B16 | B85 | QP | Uu | Xx
     deriving(Show, Eq, Data, Typeable)
 
-codecMap :: [(Codec, EncIncData -> EncIncRes String)]
+codecMap :: [(Codec, BS.ByteString -> BS.ByteString)]
 codecMap =
-    [ (B64, B64.encodeInc)
-    , (B64U, B64U.encodeInc)
-    , (B32, B32.encodeInc)
-    , (B32H, B32H.encodeInc)
-    , (B16, B16.encodeInc)
-    , (B85, B85.encodeInc)
-    , (PS, PS.encodeInc)
-    , (QP, QP.encodeInc)
-    , (Url, Url.encodeInc)
-    , (Uu, Uu.encodeInc)
-    , (Xx, Xx.encodeInc)
+    [ (B64, B64.encode)
+    , (B64U, B64U.encode)
+    , (B32, B32.encode)
+    , (B32H, B32H.encode)
+    , (B16, B16.encode)
+    , (B85, B85.encode)
+    , (QP, QP.encode)
+    , (Uu, Uu.encode)
+    , (Xx, Xx.encode)
+    -- , (PS, PS.encodeInc)
+    -- , (Url, Url.encodeInc)
     ]
 
 data MyArgs = MyArgs { argInput :: Maybe FilePath, argOutput :: Maybe FilePath, argCodec :: Codec }
@@ -73,7 +72,7 @@ myArgs :: MyArgs
 myArgs = MyArgs
     { argInput = Nothing &= name "i" &= name "in" &= explicit &= typFile &= help "read data from file"
     , argOutput = Nothing &= name "o" &= name "out" &= explicit &= typFile &= help "write encoded data to file"
-    , argCodec = B64 &= name "c" &= name "codec" &= explicit &= typ "CODEC" &= help "codec b64, b64u, b32, b32h, b16, b85, ps, qp, url, uu, xx (b64)"
+    , argCodec = B64 &= name "c" &= name "codec" &= explicit &= typ "CODEC" &= help "codec b64, b64u, b32, b32h, b16, b85, qp, uu, xx (b64)"
     } &= summary ver &= details
         [ "Encoder tool for multiple encodings:"
         , " b64  - base64 (default)"
@@ -82,42 +81,33 @@ myArgs = MyArgs
         , " b32h - base32hex"
         , " b16  - base16"
         , " b85  - base85"
-        , " ps   - python string escaping"
         , " qp   - quoted printable"
-        , " url  - url encoding"
         , " uu   - uu encoding"
         , " xx   - xx encoding"
+        -- , " ps   - python string escaping"
+        -- , " url  - url encoding"
         ]
-
--- {{{1 encode enumeratee
-encEnumeratee :: Monad m => (EncIncData -> EncIncRes String) -> Enumeratee m a
-encEnumeratee encF iter = Iteratee $ step encF iter
-    where
-        step f i Eof = let
-                EFinal ebs = f EDone
-            in do
-                ir <- runIteratee i (Chunk $ BSC.pack ebs)
-                case ir of
-                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
-                    NeedAnotherChunk i' -> do
-                        ir' <- runIteratee i' Eof
-                        case ir' of
-                            Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a Eof) Eof
-                            NeedAnotherChunk _ -> error "encEnumeratee: inner iteratee diverges on Eof"
-        step f i (Chunk bs) = let
-                EPart ebs f' = f (EChunk $ BS.unpack bs)
-            in do
-                ir <- runIteratee i (Chunk $ BSC.pack ebs)
-                case ir of
-                    Done a _ -> return $ Done (Iteratee $ \ _ -> return $ Done a (Chunk BS.empty)) (Chunk BS.empty)
-                    NeedAnotherChunk i' -> return $ NeedAnotherChunk $ Iteratee $ step f' i'
 
 -- {{{1 main
 main :: IO ()
 main = do
     cmdArgs myArgs >>= \ a -> do
-    hIn <- maybe (return stdin) (\ fn -> openFile fn ReadMode) (argInput a)
-    hOut <- maybe (return stdout) (\ fn -> openFile fn WriteMode) (argOutput a)
-    let eI = fromJust $ lookup (argCodec a) codecMap
-    (enumHandle hIn $ encEnumeratee eI (sinkHandle hOut)) >>= run >>= run
-    hClose hOut
+    let encFunc = lookup (argCodec a) codecMap
+    withMaybeFile (argInput a) ReadMode $ \ inputFile ->
+        withMaybeFile (argOutput a) WriteMode $ \ outputFile ->
+            encodeFile (fromJust encFunc) inputFile outputFile
+
+withMaybeFile :: Maybe FilePath -> IOMode -> (Handle -> IO r) -> IO r
+withMaybeFile fn mode func = let
+        dH = if mode == ReadMode
+            then stdin
+            else stdout
+    in bracket 
+        (maybe (return dH) (flip openFile mode) fn)
+        hClose
+        func
+
+encodeFile :: (BS.ByteString -> BS.ByteString) -> Handle -> Handle -> IO ()
+encodeFile encFunc inF outF = do
+    inData <- BS.hGet inF 4096
+    unless (BS.null inData) $ BS.hPut outF (encFunc inData) >> encodeFile encFunc inF outF
